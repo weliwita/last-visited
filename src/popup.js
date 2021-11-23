9258882a1eace76016b9feb9d03b18a1ff0d9e73
Rasika Weliwita <rasika@creativesoftware.com>
2021-11-23 13:32:19 +0530

    popup fixes

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,37 @@
 let addBookmark = document.getElementById("addBookmark");
 
-addBookmark.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+let itemList = document.getElementById("bookmarks");
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageUrl,
-  });
+
+
+chrome.storage.sync.get(null, function(items) {
+  var allKeys = Object.keys(items);
+  for (let key of allKeys){
+    var url = key + items[key];
+    var template = `<li>
+                        <a href=${url}>${key}</a>
+                    </li>`;
+    itemList.insertAdjacentHTML('beforeend', template);
+  }
+
+  var elements = document.getElementsByTagName('a');
+  for(var i = 0, len = elements.length; i < len; i++) {
+      elements[i].onclick = async (event) => {
+        chrome.tabs.update({url: event.target.getAttribute("href")});
+        event.preventDefault();
+        return false;
+      }
+  }
+});
+
+addBookmark.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: setPageUrl,
+    });
+
+    location.reload();
 });
 
